8873ac58682489597d587a260852f10d34d15278
clark-obama <whdlgka@gmail.com>
2025-07-26 07:45:40 +0900

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  saveData: (data) => ipcRenderer.invoke('save-data', data),
-  loadData: () => ipcRenderer.invoke('load-data'),
-  onRequestSave: (callback) => ipcRenderer.on('request-save', callback),
-  onRequestLoad: (callback) => ipcRenderer.on('request-load', callback),
-}); 
